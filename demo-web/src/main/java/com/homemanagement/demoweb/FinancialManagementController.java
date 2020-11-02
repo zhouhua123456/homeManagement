485d845651073d09d16b265784de842fe767f27c
zhouhua123456 <626602634@qq.com>
2020-11-02 21:32:34 +0800

    学无止境

--- demo-web/src/main/java/com/homemanagement/demoweb/FinancialManagementController.java
+++ demo-web/src/main/java/com/homemanagement/demoweb/FinancialManagementController.java
@@ -1,16 +1,23 @@
 package com.homemanagement.demoweb;
 
 import com.homemanagement.demoentity.entity.SeasUser;
+import com.homemanagement.demoentity.entity.UserExpenditure;
+import com.homemanagement.demoentity.entity.UserIncome;
 import com.homemanagement.demoentity.entity.UserMoney;
 import com.homemanagement.demoservice.service.ISeasUserService;
+import com.homemanagement.demoservice.service.IUserExpenditureService;
+import com.homemanagement.demoservice.service.IUserIncomeService;
 import com.homemanagement.demoservice.service.IUserMoneyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 @RestController()
 @RequestMapping("/userMoney")
@@ -21,6 +28,12 @@ public class FinancialManagementController {
 
     @Autowired
     IUserMoneyService userMoneyService;
+
+    @Autowired
+    IUserIncomeService userIncomeService;
+
+    @Autowired
+    IUserExpenditureService userExpenditureService;
     //
     @CrossOrigin(origins = "*")
     @RequestMapping("/financialManagementTab")
@@ -29,8 +42,63 @@ public class FinancialManagementController {
         SeasUser seasUser = (SeasUser)session.getAttribute("seasUser");
         System.out.println(seasUser.getUserId());
         UserMoney userMoney = userMoneyService.getUserMoneyByUserId(seasUser.getUserId());
-
         session.setAttribute("userMoney",userMoney);
         return userMoney;
+    }
+
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping("/addInComeMoney")
+    public void addInComeMoney(@RequestParam(value = "incomeAmount",required = false) String incomeAmount,@RequestParam(value = "incomeType",required = false) String incomeType,@RequestParam(value = "incomeDate",required = false) String incomeDate,@RequestParam(value = "remarks",required = false) String remarks,HttpSession session){
+        SeasUser seasUser = (SeasUser)session.getAttribute("seasUser");//从session中获取用户
+        UserMoney userMoney = userMoneyService.getUserMoneyByUserId(seasUser.getUserId());//根据用户id查询出总金额
+        UserIncome userIncome = new UserIncome();
+        userIncome.setUserId(seasUser.getUserId());
+        userIncome.setUserName(seasUser.getUserName());
+        userIncome.setIncomeAmount(incomeAmount);
+        userIncome.setIncomeType(incomeType);
+        incomeDate = incomeDate.replace("Z", " UTC");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z");
+        SimpleDateFormat defaultFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        java.sql.Date date1 = null;
+        try{
+            java.util.Date date = format.parse(incomeDate);
+            date1 = new Date(date.getTime());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        userIncome.setIncomeDate(date1);
+        userIncome.setRemarks(remarks);
+        userIncomeService.addIncomeRecord(userIncome);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping("/addExpenditureRecords")
+    public String addExpenditureRecords(@RequestParam(value = "expenditureAmount",required = false) String expenditureAmount,@RequestParam(value = "expenditureType",required = false) String expenditureType,@RequestParam(value = "expenditureDate",required = false) String expenditureDate,@RequestParam(value = "expenditureExplain",required = false) String expenditureExplain,HttpSession session){
+        SeasUser seasUser = (SeasUser)session.getAttribute("seasUser");//从session中获取用户
+        UserExpenditure userExpenditure = new UserExpenditure();
+        userExpenditure.setUserId(seasUser.getUserId());
+        userExpenditure.setUserName(seasUser.getUserName());
+        userExpenditure.setExpenditureAmount(expenditureAmount);
+        userExpenditure.setExpenditureType(expenditureType);
+        userExpenditure.setExpenditureExplain(expenditureExplain);
+        expenditureDate = expenditureDate.replace("Z", " UTC");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z");
+        SimpleDateFormat defaultFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        java.sql.Date date1 = null;
+        try{
+            java.util.Date date = format.parse(expenditureDate);
+            date1 = new Date(date.getTime());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        userExpenditure.setExpenditureDate(date1);
+
+        try {
+            userExpenditureService.addExpenditureRecords(userExpenditure);
+        }catch (Exception  e){
+            return "0";
+        }
+        return "1";
     }
 }
--- demo-web/src/main/java/com/homemanagement/demoweb/SeasUserController.java
+++ demo-web/src/main/java/com/homemanagement/demoweb/SeasUserController.java
@@ -1,13 +1,21 @@
 package com.homemanagement.demoweb;
 
 import com.homemanagement.demoentity.entity.SeasUser;
+import com.homemanagement.demoentity.entity.UserMoney;
 import com.homemanagement.demoservice.service.ISeasUserService;
+import com.homemanagement.demoservice.service.IUserMoneyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+import java.util.Map;
 
 @RestController
 public class SeasUserController {
@@ -15,21 +23,31 @@ public class SeasUserController {
     @Autowired
     ISeasUserService seasUserService;
 
+    @Autowired
+    IUserMoneyService userMoneyService;
+
     @CrossOrigin(origins = "*")
     @RequestMapping("/login")
-    public SeasUser querySeasUser(@RequestParam(value = "username",required = false) String username, @RequestParam(value = "password",required = false) String password, Model model){
+    public SeasUser querySeasUser(@RequestParam(value = "username",required = false) String username, @RequestParam(value = "password",required = false) String password, Model model, HttpSession session, HttpServletRequest request){
         System.out.println("用户名为："+username);
         System.out.println("密码为："+password);
-        SeasUser seasUser = seasUserService.getSeasUserByUserId(username);
+
+        SeasUser seasUser = seasUserService.getSeasUserByUserId(username);//这里需要将个人信息缓存一下
+        userMoneyService.getUserMoneyByUserId(username);//这边需要将个人的总财产缓存一下
         if(seasUser != null){
             if(password.equals(seasUser.getPassword())){
+
+                session.setAttribute("seasUser",seasUser);
+                String id = session.getId();
+                System.out.println(id);
                 return seasUser;
             }else{
                 seasUser = null;
             }
         }
-        System.out.println("123");
         //SeasUser seasUser = seasUserService.getSeasUser(id);
         return seasUser;
     }
+
+
 }
